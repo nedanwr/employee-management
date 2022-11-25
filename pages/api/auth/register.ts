27e269c8df218b1c9c98b10584fea49a677728b9
Naveed <46922100+nedanwr@users.noreply.github.com>
2022-11-25 21:24:06 +0500

    (Feat): create user in db after all checks pass

--- pages/api/auth/register.ts
+++ pages/api/auth/register.ts
@@ -2,10 +2,11 @@ import type { NextApiRequest, NextApiResponse } from "next";
 import { validate as validateEmail } from "email-validator";
 import { validatePassword } from "../../../utils/validatePassword";
 import { prisma } from "../../../prisma";
+import bcryptjs from "bcryptjs";
 
 const registerHandler = async (req: NextApiRequest, res: NextApiResponse) => {
     // Get the data from the request body
-    const { email, full_name, username, password } = req.body;
+    const { email, full_name, password } = req.body;
 
     // Validate email
     if (!validateEmail(email)) {
@@ -45,6 +46,31 @@ const registerHandler = async (req: NextApiRequest, res: NextApiResponse) => {
                 message: "Email already exists"
             });
     }
+
+    // Create user in database
+    await prisma
+        .user
+        .create({
+            data: {
+                email,
+                name: full_name,
+                password: await bcryptjs.hash(password, 12),
+                createdAt: new Date().getTime(),
+            }
+        })
+        .then(() => {
+            return res
+                .json({
+                    statusCode: 201,
+                    message: "User created successfully"
+                });
+        })
+        .catch((error: any | unknown) => {
+            throw new Error(error.message);
+        })
+        .finally(() => {
+            return prisma.$disconnect();
+        });
 };
 
 export default registerHandler;
