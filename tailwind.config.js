8aaa405d00b4626f789a105abb4b473da0ab2608
Naveed <46922100+nedanwr@users.noreply.github.com>
2022-11-25 18:05:54 +0500

    (Chore): init tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+      "./pages/**/*.{ts,tsx}",
+      "./modules/**/*.{ts,tsx}",
+      "./ui/**/*.{ts,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
