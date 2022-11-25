422d40aca1f043a0ccc2ff4af5a84c9961c3bce0
Naveed <46922100+nedanwr@users.noreply.github.com>
2022-11-25 20:02:41 +0500

    (Feat): create `Button` ui component

---
+++ ui/Button.tsx
@@ -0,0 +1,42 @@
+import React, {
+    FC,
+    ButtonHTMLAttributes,
+    DetailedHTMLProps,
+    ReactNode
+} from "react";
+
+export type ButtonProps = DetailedHTMLProps<
+    ButtonHTMLAttributes<HTMLButtonElement>,
+    HTMLButtonElement
+> & {
+    loading?: boolean;
+    icon?: ReactNode;
+    variant?: "default";
+};
+
+const Button: FC<ButtonProps> = ({
+    children,
+    disabled,
+    loading,
+    icon,
+    variant,
+    className,
+    ...props
+}) => {
+    return (
+        <button
+            disabled={disabled || loading}
+            className={
+                variant === "default"
+                    ? `bg-blue-600 text-white py-3.5 rounded w-full mt-4 font-medium hover:bg-blue-700 duration-300 ease-linear ${className}`
+                    : className
+            }
+            data-testid="button"
+            {...props}
+        >
+            <span className={`text-center`}>{children}</span>
+        </button>
+    );
+};
+
+export default Button;
